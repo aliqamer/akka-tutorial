563295bcf7d43b384760acdae7ce7438fbb772d7
ali_aus <dev@investmentor.com.au>
2020-12-31 23:19:10 +1100

    first commit

---
+++ src/main/java/com/example/Main.java
@@ -0,0 +1,21 @@
+package com.example;
+
+import akka.actor.typed.ActorSystem;
+
+public class Main {
+
+    /*public static void main(String[] args) {
+        ActorSystem<String> actorSystem = ActorSystem.create(FirstSimpleBehavior.create(), "FirstActorSystem");
+
+        actorSystem.tell("say hello");
+        actorSystem.tell("who are you");
+        actorSystem.tell("create a child");
+        actorSystem.tell("are you there?");
+
+    }*/
+
+    public static void main(String[] args) {
+        ActorSystem<String> managerActor = ActorSystem.create(ManagerBehavior.create(), "ManagerActor");
+        managerActor.tell("start");
+    }
+}
---
+++ src/main/java/com/example/ManagerBehavior.java
@@ -0,0 +1,35 @@
+package com.example;
+
+import akka.actor.typed.ActorRef;
+import akka.actor.typed.Behavior;
+import akka.actor.typed.javadsl.AbstractBehavior;
+import akka.actor.typed.javadsl.ActorContext;
+import akka.actor.typed.javadsl.Behaviors;
+import akka.actor.typed.javadsl.Receive;
+
+import java.math.BigInteger;
+import java.util.Random;
+
+public class ManagerBehavior extends AbstractBehavior<String> {
+
+    private ManagerBehavior(ActorContext<String> context) {
+        super(context);
+    }
+
+    public static Behavior<String> create() {
+        return Behaviors.setup(ManagerBehavior::new);
+    }
+
+    @Override
+    public Receive<String> createReceive() {
+        return newReceiveBuilder()
+                .onMessageEquals("start", () -> {
+                    for (int i = 1; i <= 20; i++) {
+                        ActorRef<String> actorRef = getContext().spawn(WorkerBehavior.create(), "Worker-" + i);
+                        actorRef.tell("start");
+                    }
+                    return this;
+                })
+                .build();
+    }
+}
